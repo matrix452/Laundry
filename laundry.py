481d1139160f027a2104208968eea1b80732e667
Kevin <matrix452@gmail.com>
2021-06-20 22:54:11 -0400

    Added Name and PIN Variables

--- laundry.py
+++ laundry.py
@@ -1,19 +1,62 @@
-def (detergent, softener, bleech, clorox2, woollite):
+def soaps(detergent, softener, bleech, clorox2, woollite):
+	pin_to_open_1 = detergent
+	pin_to_open_2 = softener
+	pin_to_open_3 = bleech
+	pin_to_open_4 = clorox2
+	pin_to_open_5 = woollite
 
 def colors:
 	#here we measure out what we need:
 	clorox2 = 0
 	softener = 12
 	detergent = 12
+	bleech = 0
+	woollite = 0
+	soaps(detergent,softener,bleech,clorox2,woollite)
 
 
 def blues:
+	#here we measure out what we need:
+	clorox2 = 0
+	softener = 12
+	detergent = 12
+	bleech = 0
+	woollite = 0
+	soaps(detergent,softener,bleech,clorox2,woollite)
 
 def delecates:
+	#here we measure out what we need:
+	clorox2 = 0
+	softener = 0
+	detergent = 0
+	bleech = 0
+	woollite = 12
+	soaps(detergent,softener,bleech,clorox2,woollite)
 
 def whites:
+	#here we measure out what we need:
+	clorox2 = 0
+	softener = 12
+	detergent = 12
+	bleech = 12
+	woollite = 0
+	soaps(detergent,softener,bleech,clorox2,woollite)
 
 def darks:
+	#here we measure out what we need:
+	clorox2 = 0
+	softener = 12
+	detergent = 12
+	bleech = 0
+	woollite = 12
+	soaps(detergent,softener,bleech,clorox2,woollite)
 
 def towels:
+	#here we measure out what we need:
+	clorox2 = 12
+	softener = 0
+	detergent = 12
+	bleech = 0
+	woollite = 0
+	soaps(detergent,softener,bleech,clorox2,woollite)
 

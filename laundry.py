fa3f40dfb32f2fb0c3390c7d0575bfceef82a159
Kevin <matrix452@gmail.com>
2021-06-20 22:42:57 -0400

    initial upload

---
+++ laundry.py
@@ -0,0 +1,19 @@
+def (detergent, softener, bleech, clorox2, woollite):
+
+def colors:
+	#here we measure out what we need:
+	clorox2 = 0
+	softener = 12
+	detergent = 12
+
+
+def blues:
+
+def delecates:
+
+def whites:
+
+def darks:
+
+def towels:
+
